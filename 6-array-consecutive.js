1218bac1f18e18517c6dd33ef13a57259c26a229
Finn Thye <piqueen@gmail.com>
2021-02-23 07:36:55 -0700

    Solved 6-array-consecutive.js

--- 6-array-consecutive.js
+++ 6-array-consecutive.js
@@ -1,7 +1,10 @@
 // https://app.codesignal.com/arcade/intro/level-2/bq2XnSr5kbHqpHGJC
 
 function makeArrayConsecutive2(statues) {
-
+ const max = Math.max(...statues)
+ const min = Math.min(...statues)
+ const filledLength = max - min
+ return filledLength - statues.length + 1
 }
 
-console.log(makeArrayConsecutive2([6, 2, 3, 8]))
+console.log(makeArrayConsecutive2([6, 2, 3, 8])) // 3
