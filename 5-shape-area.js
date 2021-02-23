c7d82725f3e857e2ec4af6c875723925949f0d76
Finn Thye <piqueen@gmail.com>
2021-02-22 21:24:24 -0700

    Solved 5-shape-area.js

--- 5-shape-area.js
+++ 5-shape-area.js
@@ -1,9 +1,7 @@
 // https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ
 
 function shapeArea(n) {
-   const base = 1
-   let current = shapeArea(n-1) + 4*(n-1)
-   return current
-  }
+  return Math.pow(n,2) + Math.pow(n-1,2)
+}
 
   console.log(shapeArea(5)) // 41
