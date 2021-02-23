4e0c8691e8c7075edf9ccd1ecc0b1cf61435b6b4
Finn Thye <piqueen@gmail.com>
2021-02-22 20:39:00 -0700

    Solved adjacent-elements.js

--- adjacent-elements.js
+++ adjacent-elements.js
@@ -1,14 +1,13 @@
 function adjacentElementsProduct(inputArray) {
-  let max = 0
-  for (let i=0; i<inputArray - 1; i++) {
+  let max = inputArray[0]*inputArray[1]
+  for (let i=1; i<inputArray.length - 1; i++) {
       let current = inputArray[i]*inputArray[i+1]
-          console.log(i)
       if (current > max) {
-          console.log(current)
+          max = current
       }
   }
   
   return max
 }
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
+console.log(adjacentElementsProduct([-23, 4, -3, 8, -12]))
