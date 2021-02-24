e3d10b02ef57da137e31d67f5d245ce9422af213
Finn Thye <piqueen@gmail.com>
2021-02-23 21:04:13 -0700

    Set up 8-matrix-sum.js

---
+++ 8-matrix-sum.js
@@ -0,0 +1,15 @@
+// https://app.codesignal.com/arcade/intro/level-2/xskq4ZxLyqQMCLshr
+
+function matrixElementsSum(matrix) {
+  let sum = 0
+  for(let i=0;i<matrix[0].length;i++) {
+      for(let j=0;j<matrix.lengh;j++) {
+          if (matrix[i][j])
+      }   
+  }
+}
+
+// for the first element of the first Array
+// -if it's not zero, add it to sum, then check first element of second Array. Repeat through the final array
+// -if it's zero skip all first elements and go to second element of first Array
+// -if it's not zero, add it to sum, then check second element of second Array. Repeat through the last array
